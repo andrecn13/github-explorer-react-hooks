c4358cb968261c558b538c78e0d242aa6f98e287
Andre Nunes <cnandre.nunes@gmail.com>
2020-06-08 23:01:06 -0300

    [feat] add repositories and save in localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './style';
+import api from '../../services/api';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories, Error } from './style';
+import Repository from '../Repository';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const StoragedRepositories = localStorage.getItem(
+      '@GithubExplorer:respositories',
+    );
+
+    if (StoragedRepositories) return JSON.parse(StoragedRepositories);
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:respositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleFormSubmit(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/repositorio!!');
+      return;
+    }
+
+    try {
+      const { data } = await api.get<Repository>(`repos/${newRepo}`);
+
+      setRepositories([...repositories, data]);
+      setNewRepo('');
+    } catch (error) {
+      setInputError('Erro ao buscar repositório, tente novamente!');
+    }
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form hasError={!!inputError} onSubmit={handleFormSubmit}>
+        <input
+          placeholder="Digite o nome do usuário/repositorio"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="tete">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={22} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
 };
 export default Dashboard;
